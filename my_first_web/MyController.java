effe6fe1756ad55fc0d2c8f0ca52a730aff3e316
qa191120 <74743291+qa191120@users.noreply.github.com>
2021-05-03 20:27:14 +0300

    Update MyController.java

--- my_first_web/MyController.java
+++ my_first_web/MyController.java
@@ -6,8 +6,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyController {
+
     @GetMapping("/hello")
-    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
+    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name)
+    {
         return String.format("<h1 style=\"color:red\">Hello %s!</h1><h2>smaller</h2>", name);
     }
+    @GetMapping("/add")
+    public String add(@RequestParam(value = "a", defaultValue = "0") Integer a,
+                      @RequestParam(value = "b", defaultValue = "0") Integer b)
+    {
+        return String.format("%d + %d = %d",a, b, a+b);
+    }
+
+    // exercise:
+    // 1. multiply
+    // 2. div -- when b is 0 then print error (in red)
 }
